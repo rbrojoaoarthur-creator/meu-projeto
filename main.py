6cc2b36f79c0407908435eb2e95e69dfb695e3b5
rbrojoaoarthur-creator <rbro.joaoarthur@gmail.com>
2026-05-25 18:24:49 -0300

    Implement word lookup in meme dictionary
    Add a dictionary of phrases with user input handling.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+word = input("digite uma palavra que voce disconhe para descobrir o resultado, escreva em letra maiuscula").upper()
 
+meme_dict = {
+            "AURA": "é como ganhar uma estrelinha, faz algo legal",
+            "67": "é o nome de um time de basquete (meu esporte favorito) e significa 2 metros de altura em pes",
+    "SIGMA": "pessoa muito top/legal"
+            }
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("eita essa ate nois disconhece, adolecentes loucos kkk")
