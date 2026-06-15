b418f74ddf9142531a28351e2f80a55794d350ee
rbrojoaoarthur-creator <rbro.joaoarthur@gmail.com>
2026-06-15 18:21:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-word = input("digite uma palavra que voce disconhe para descobrir o resultado, escreva em letra maiuscula").upper()
+import discord
+from discord.ext import commands
+import random
 
-meme_dict = {
-            "AURA": "é como ganhar uma estrelinha, faz algo legal",
-            "67": "é o nome de um time de basquete (meu esporte favorito) e significa 2 metros de altura em pes",
-    "SIGMA": "pessoa muito top/legal"
-            }
+description = '''An example bot to showcase the discord.ext.commands extension
+module.'''
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("eita essa ate nois disconhece, adolecentes loucos kkk")
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('------'*10)
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------'*10)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run('Insira seu token')
